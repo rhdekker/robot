d0f5323d1b521f601d77fdf9efd4daaf614ff6b0
Ronald Haentjens Dekker <ronald.dekker@huygens.knaw.nl>
2015-07-09 10:58:35 +0200

    WorldVectorTest: added assert.

--- src/test/java/world/WorldVectorTest.java
+++ src/test/java/world/WorldVectorTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by ronalddekker on 08/07/15.
  */
@@ -11,12 +13,9 @@ public class WorldVectorTest {
 
     @Test
     public void testRasterizeBorders() {
-        WorldVector vector = new WorldVector(new Point(0, 0), new Point(19, 0));
+        WorldVector vector = new WorldVector(new Point(0, 0), new Point(5, 0));
         WorldObject upperBorder = new WorldObject("upper", vector);
         List<Point> points = vector.rasterize();
-        System.out.println(points);
-
-
-
+        assertEquals("[(0,0), (1,0), (2,0), (3,0), (4,0), (5,0)]", points.toString());
     }
 }
